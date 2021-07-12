6ca29e25713186a73feab0ee39743657e5011300
SJLEE316 <lsj8249@naver.com>
2021-07-12 12:56:58 +0900

    Greedy, Implementation_feedback

--- Baekjoon/이소정/14916_거스름돈.py
+++ Baekjoon/이소정/14916_거스름돈.py
@@ -2,26 +2,9 @@ n = int(input())
 
 if (n == 1 or n == 3) :
   result = -1
-elif (n % 5 == 0) : # 5의 배수이면
-  five = n // 5 
-  result = five
-elif (n % 2 == 0) : # 2의 배수이면
-  two = n // 2
-  result = two
-else :
-  five = n // 5
-  two = (n % 5) // 2
-  result = five + two
+elif (n % 5) % 2 == 0 : # 7 -> 5 * 1 + 2 * 1
+  result = n // 5 + (n % 5) // 2
+else : # 8 -> 5 * 0 + 2 * 4 
+  result = ((n // 5) - 1) + ((n % 5 + 5) // 2)
+
 print(result)
-
-
-# n = int(input())
-
-# if (n == 1 or n == 3) :
-#   result = -1
-# elif (n % 5) % 2 == 0 : 
-#   result = n // 5 + (n % 5) // 2
-# else :
-#   result = ((n // 5) - 1) + ((n % 5 + 5) // 2)
-
-# print(result)
--- Baekjoon/이소정/1913_달팽이.py
+++ Baekjoon/이소정/1913_달팽이.py
@@ -1,15 +1,29 @@
 N = int(input())
 
 board = [[0] * N for i in range(N)]
-data = N*N
+data = N*N # 9
 
 # 모서리에 도달할 때마다 방향을 바꿔주면 됨
 # 25부터 거꾸로 구현
 
 for i in range(N) : # 열 같음
   for j in range(N) : # 행 달라짐
-    data = data - 1
     board[j][i] = data
-    print(data)
-
+    data = data - 1
+    if (j == N-1) :
+      for i in range(N) :
+        data = data -1
+        board[j][i] = data
 print(board)
+
+#     i=0 i=1 i=2 i=3 i=4
+# j=0  25
+# j=1  24
+# j=2  23
+# j=3  22              16
+# j=4  21  20  19  18  17
+
+#     i=0 i=1 i=2
+# j=0  9
+# j=1  8
+# j=2  7
