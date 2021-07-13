5614ebdf16c3d3e7a2317ee68ed40c14b8fbe56c
SJLEE316 <lsj8249@naver.com>
2021-07-13 22:12:19 +0900

    Baekjoon_Implementation (1913 완성)

--- Baekjoon/이소정/1913_달팽이.py
+++ Baekjoon/이소정/1913_달팽이.py
@@ -1,29 +1,61 @@
 N = int(input())
+num = int(input())
 
 board = [[0] * N for i in range(N)]
 data = N*N # 9
 
-# 모서리에 도달할 때마다 방향을 바꿔주면 됨
-# 25부터 거꾸로 구현
+# 현재 위치
+A = 0
+B = 0
 
-for i in range(N) : # 열 같음
-  for j in range(N) : # 행 달라짐
-    board[j][i] = data
-    data = data - 1
-    if (j == N-1) :
-      for i in range(N) :
-        data = data -1
-        board[j][i] = data
-print(board)
+# 현재 방향
+dir = 2
 
-#     i=0 i=1 i=2 i=3 i=4
-# j=0  25
-# j=1  24
-# j=2  23
-# j=3  22              16
-# j=4  21  20  19  18  17
+# 북 0 동 1 남 2 서3
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]
 
-#     i=0 i=1 i=2
-# j=0  9
-# j=1  8
-# j=2  7
+
+def turn_left():
+  global dir
+  if dir == 0:
+    dir = 3
+  else:
+    dir = dir -1
+
+
+go_count = 0
+turn_count = 0
+go_row = N-1
+first_turn = 0
+board[A][B] = data # 처음 위치 표시
+
+while(True):
+  A = A + dx[dir]
+  B = B + dy[dir]
+  go_count += 1
+  data -= 1
+  if (data == num):
+    x = A + 1
+    y = B + 1
+  board[A][B] = data
+  if (data == 1):
+    break;
+  if (go_count == go_row): # 2
+    turn_left()
+    turn_count += 1
+    go_count = 0
+  if (first_turn == 0 and turn_count == 3):
+    go_row = go_row - 1
+    turn_count = 0
+    first_turn = 1
+  if (first_turn == 1 and go_count == 0):
+    if turn_count == 2:
+      go_row = go_row -1
+      turn_count = 0
+
+for i in range(N):
+  for j in range(N):
+    print(board[i][j], end=' ')
+  print()
+print(x, y)
