369461357a76bbc25294650a2705c88d2c092f5e
dddooo9 <dlaehdus99@naver.com>
2021-07-15 23:26:38 +0900

    baekjoon_DFS&BFS_임도연(11725해결)

--- Baekjoon/임도연/11725_트리의부모찾기.py
+++ Baekjoon/임도연/11725_트리의부모찾기.py
@@ -1,6 +1,9 @@
 # 루트 없는 트리가 주어진다.
 # 이때, 트리의 루트를 1이라고 정했을 때, 각 노드의 부모를 구하는 프로그램을 작성하시오.
 
+# import sys
+# sys.setrecursionlimit(10**9)
+
 n = int(input())
 graph = [[] for _ in range(n + 1)]
 
@@ -8,16 +11,29 @@ for i in range(n - 1):
     a, b = map(int, input().split())
     graph[a].append(b)
     graph[b].append(a)
-print(graph)
 
 parent_node = [0] * (n + 1)
 
 
-def dfs(graph, v, visited):
-    pass
+def dfs(graph, v):
+    stack = [v]
+    while stack:
+        node = stack.pop()
+        for i in graph[node]:
+            parent_node[i] = node
+            stack.append(i)
+            graph[i].remove(node)
 
 
-array = [0] * (n + 1)
-dfs(graph, 2, array)
+# def dfs(graph, v, visited):
+#     visited[v] = True
+#     for i in graph[v]:
+#         if not visited[i]:
+#             parent_node[i] = v
+#             dfs(graph, i, visited)
+
+
+dfs(graph, 1)
+
 for i in range(2, n + 1):
-    print()
+    print(parent_node[i])
