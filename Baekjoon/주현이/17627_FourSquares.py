ca39458570d880e9c38835620a011627dffdb014
hyeoneedyou <hyeoneedyou@naver.com>
2021-08-05 22:46:47 +0900

    Four Squares 해결

--- Baekjoon/주현이/17627_FourSquares.py
+++ Baekjoon/주현이/17627_FourSquares.py
@@ -1,13 +1,12 @@
+import math
 n = int(input())
-d = [50001] * (n+1)
-d[0] = 0
+d = [5] * (n+1)
+d[1] = 1
 
-for i in range(n):
-    for j in range(i**2, (n**2)+1):
-        if d[j] != 50001:
-            d[j] = min(d[j], d[j-n**2]+1)
+for i in range(2, n + 1):
+    if math.sqrt(i).is_integer():
+        d[i] = 1
+    for j in range(1, int(math.sqrt(i)) + 1):
+        d[i] = min(d[i], d[i - j**2] + d[j**2])
 
-if d[n] == 50001:
-    print(-1)
-else:
-    print(d[n])
+print(d[n])
