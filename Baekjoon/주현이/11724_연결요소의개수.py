7b6cf55eac9c1b85a19dee12be918add519ce5e3
hyeoneedyou <hyeoneedyou@naver.com>
2021-10-10 23:47:23 +0900

    11724_연결요소의개수 solution

--- Baekjoon/주현이/11724_연결요소의개수.py
+++ Baekjoon/주현이/11724_연결요소의개수.py
@@ -1,5 +1,6 @@
 import sys
 sys.setrecursionlimit(10000)
+
 n, m = map(int, input().split())
 # 연결된 정점 저장
 graph = [[] for _ in range(n+1)]
@@ -9,25 +10,22 @@ for _ in range(m):
     graph[u].append(v)
     graph[v].append(u)
 #  체크 여부
-is_visited = [0] * (n+1)
+visited = [0] * (n+1)
+
 
 #  연결 요소 계산
-def cnt(i, res, is_visited):
-    if is_visited[i] == 1:
-        return res, is_visited
-    if len(graph[i]) == 0:
-        is_visited[i] = 1
-        res += 1
-        return res, is_visited
-    is_visited[i] = 1
-    for x in graph[i]:
-        cnt(x, res, is_visited)
-    res += 1
-    return res, is_visited
+def dfs(p):
+    visited[p] = True
+    for x in graph[p]:
+        if not visited[x]:
+            dfs(x)
 
 
 ans = 0
 for i in range(1, n+1):
-    ans, is_visited = cnt(i, ans, is_visited)
+    if not visited[i]:
+        dfs(i)
+        ans += 1
 
 print(ans)
+
