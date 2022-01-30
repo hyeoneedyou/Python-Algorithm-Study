c53cd5f3e9bf68aec9fcd6e85d0bfbf1e30a74ad
dddooo9 <dlaehdus99@naver.com>
2022-01-30 23:13:52 +0900

    baekjoon_9250 풀이 확인

--- Baekjoon/임도연/9250_맥주마시면서걸어가기.py
+++ Baekjoon/임도연/9250_맥주마시면서걸어가기.py
@@ -3,13 +3,20 @@ def get_distance(from_position, to_position):
     return abs(from_position[0] - to_position[0]) + abs(from_position[1] - to_position[1])
 
 
-def go(xy):
-    if xy[0] == festival[0] and xy[1] == festival[1]:
-        print("happy")
-        return
-    for i in range(n + 1):
-        if get_distance(xy, destination[i]) <= 1000:
-            go(destination[i])
+def bfs():
+    visited = []
+    q = [house]
+    while q:
+        now = q[0]
+        del q[0]  # 방문 노드 삭제
+        if now not in visited:
+            visited.append(now)
+            if get_distance(now, festival) <= 1000:
+                print("happy")
+                return
+            for i in range(n):
+                if get_distance(now, store[i]) <= 1000:
+                    q.append(store[i])
     print("sad")
 
 
@@ -21,7 +28,4 @@ for _ in range(t):
     store = [list(map(int, input().split())) for _ in range(n)]  # 편의점 좌표
     festival = list(map(int, input().split()))  # 페스티벌 좌표
 
-    destination = store
-    destination.append(festival)
-    road = [0 * (n + 2)]
-    go(house)
+    bfs()
