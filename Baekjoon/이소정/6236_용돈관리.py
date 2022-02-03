3182b3f91ef339ecc1464d98d390571fdf592da3
unknown <lsj8249@naver.com>
2022-02-03 23:28:06 +0900

    6236_용돈관리

--- Baekjoon/이소정/6236_용돈관리.py
+++ Baekjoon/이소정/6236_용돈관리.py
@@ -2,16 +2,15 @@
 # M번
 # K원
 
-n, m = map(int, input().split())
-prices = list(int(input()) for _ in range(n))
-
-
 # 1000원 인출
 # 100 400 300 100 500 101 400
 # 900 500 200 100
 #                 500 399
 #                         600
 # 3번 인출 < 5번 -> 인출 금액 줄여야 함
+
+n, m = map(int, input().split())
+prices = list(int(input()) for _ in range(n))
 
 left = min(prices)
 right = sum(prices)
@@ -29,9 +28,10 @@ def binary():
 while(left <= right):
     mid = (left + right) // 2
     cnt = binary()
-    if cnt <= m: # 인출 금액이 큼 -> 줄이자
-        right = mid - 1
-    elif cnt > m or mid < max(prices): # 인출 금액이 작음 -> 키우자
+    # 이게 문제...!!ㅠㅠㅠ
+    if cnt > m or mid < max(prices): # 인출 금액이 작음 -> 키우자
         left = mid + 1
+    else: # 인출 금액이 큼 -> 줄이자
+        right = mid - 1
 
 print(left)
